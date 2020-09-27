1ec6bc3a4445c9aba624af4d79091358b61b4fa4
drewkiimon <andrew.r.pagan@gmail.com>
2020-09-27 09:00:13 -0700

    added timestamp to schema

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
-const todoSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true,
+const todoSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		userId: {
+			type: String,
+			required: true,
+		},
 	},
-	userId: {
-		type: String,
-		required: true,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 module.exports = Todo = mongoose.model('todo', todoSchema);
